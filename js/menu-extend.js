45f983ec18571820a9bee3546b0787ea505b1f03
Noah Kobayashi <vohedge@gmail.com>
2012-09-10 23:01:35 +0900

    Add menu meta data save function

--- js/menu-extend.js
+++ js/menu-extend.js
@@ -1,4 +1,5 @@
 var AddExtraMenuOption = function() {
+	this.saved_data_icons = ( typeof( menu_item_data_icons ) != 'undefined' ) ? menu_item_data_icons : '';
 	this.menu_items = jQuery( '#menu-to-edit>li' );
 	this.init();
 	this.new_menu();
@@ -16,11 +17,12 @@ AddExtraMenuOption.prototype = {
 			var j = jQuery( this );
 			if ( ! j.find( 'p.field-data-icon' ).size() ) {
 				var id = self.get_menu_id( j );
+				var data_icon = self.saved_data_icons[id] || '';
 				var field = '<p class="field-data-icon description description-thin">';
 				field += '<label for="edit-menu-iteme-data-icon-' + id + '">';
-				field += 'Web Font Icon Data (unicode)<br />'
+				field += '"data-icon"<br />'
 				field += '</label>';
-				field += '<input type="text" id="edit-menu-iteme-data-icon-' + id + '" class="widefat code edit-menu-item-icon" name="menu-item-data-icon[' + id + ']" />';
+				field += '<input type="text" id="edit-menu-iteme-data-icon-' + id + '" class="widefat code edit-menu-item-icon" name="menu-item-data-icon[' + id + ']" value="' + data_icon + '" />';
 				field += '</p>';
 				j.find( 'div.menu-item-settings>p.field-css-classes' ).after( field );
 			}
