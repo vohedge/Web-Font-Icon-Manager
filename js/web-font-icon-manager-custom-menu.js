085e5d69aa740de4aaba52b8f112e0b88a10d2c6
Noah Kobayashi <vohedge@gmail.com>
2012-09-25 14:05:59 +0900

    Beta version completed

--- js/web-font-icon-manager-custom-menu.js
+++ js/web-font-icon-manager-custom-menu.js
@@ -1,5 +1,6 @@
 var AddExtraMenuOption = function() {
 	this.saved_data_icons = ( typeof( menu_item_data_icons ) != 'undefined' ) ? menu_item_data_icons : '';
+	this.saved_data_icons_classes = ( typeof( menu_item_data_icons_classes ) != 'undefined' ) ? menu_item_data_icons_classes : '';
 	this.i18n = ( typeof( wfim_cm_i18n ) != 'undefined' ) ? wfim_cm_i18n : '';
 	this.menu_items = jQuery( '#menu-to-edit>li' );
 	this.init();
@@ -18,12 +19,18 @@ AddExtraMenuOption.prototype = {
 			var j = jQuery( this );
 			if ( ! j.find( 'p.field-data-icon' ).size() ) {
 				var id = self.get_menu_id( j );
-				var data_icon = self.saved_data_icons[id] || '';
+				var code_point = self.saved_data_icons[id] || '';
+				var data_icon = code_point ? '&#' + code_point + ';' : '';
+				var font_name = self.saved_data_icons_classes[id] || '';
+				var class_name = font_name ? ' icon-' + font_name : '';	
 				var field = '<p class="field-data-icon description description-thin">';
 				field += '<label for="edit-menu-iteme-data-icon-' + id + '">';
 				field += self.i18n['Icon'] + '<br />'
 				field += '</label>';
-				field += '<input type="text" id="edit-menu-iteme-data-icon-' + id + '" class="widefat code edit-menu-item-icon" name="menu-item-data-icon[' + id + ']" value="' + data_icon + '" />';
+				field += '<input type="button" class="wfim_icon_select" value="' + self.i18n['Select Icon'] + '" />';
+				field += '<input type="hidden" id="edit-menu-iteme-data-icon-' + id + '" class="widefat code data_icon" name="menu-item-data-icon[' + id + ']" value="' + code_point + '" />';
+				field += '<input type="hidden" id="edit-menu-iteme-data-icon-class-' + id + '" class="widefat code font_name" name="menu-item-data-icon-class[' + id + ']" value="' + font_name + '" />';
+				field += '<span class="icon_preview' + class_name+ '">' + data_icon + '</span>';
 				field += '</p>';
 				j.find( 'div.menu-item-settings>p.field-css-classes' ).after( field );
 			}
---
+++ js/web-font-icon-manager-icon-selector.js
@@ -0,0 +1,76 @@
+var wfim_font_selector = function() {
+	this.icon_set = jQuery( '<div id="wfim_icon_set"></div>' ).hide();
+	this.init();
+
+	// Event actions
+	this.open_icon_selector();
+	this.insert_code_point();
+	this.remove_after_insert();
+	this.thick_box_height_fix();
+}
+wfim_font_selector.prototype = {
+	init : function() {
+		this.set_code_points();
+		jQuery( 'body' ).append( this.icon_set );
+	},
+	set_code_points : function() {
+		if ( ! wfim_fonts )
+			return;
+
+		var fonts = wfim_fonts;
+		var code_points;
+		var output = '<ul id="wfim_icon_list">';
+		for ( var font in fonts ) {
+			code_points = fonts[font];
+			for ( var i = 0; i < code_points.length; i++ ) {
+				output += '<li><a href="" class="icon-' + font + '" title="' + code_points[i] + '">&#' + code_points[i] + '</a></li>';
+			}
+		}
+		output += '</ul>';
+		this.icon_set.append( output );
+	},
+	open_icon_selector : function() {
+		var self = this;
+		jQuery( 'input.wfim_icon_select' ).live( 'click', function() {
+			self.current_item = jQuery( this );
+			tb_show( wfim_cm_i18n['Icon'], '#TB_inline?inlineId=wfim_icon_set&width=640&height=480' );
+			self.fix_thickbox_height();
+		});
+	},
+	insert_code_point : function() {
+		var self = this;
+		jQuery( '#wfim_icon_list>li>a' ).live( 'click', function() {
+			var code_point = jQuery( this ).attr( 'title' );
+			var font_name = jQuery( this ).attr( 'class' );
+			font_name = font_name.replace( /^icon-/, '' );
+			if ( self.current_item.size() > 0 ) {
+				var parent_elm = self.current_item.closest( '.field-data-icon' );
+				parent_elm.children( 'span.icon_preview' ).text( '' ).append( '&#' + code_point + ';' ).addClass( 'icon-' + font_name );
+				parent_elm.children( 'input.data_icon' ).val( code_point );
+				parent_elm.children( 'input.font_name' ).val( font_name );
+			}
+			tb_remove();
+			return false;
+		});
+	},
+	remove_after_insert : function() {
+		jQuery( document ).ajaxComplete( function() {
+			jQuery( '.field-data-icon span.icon_preview' ).text( '' );
+		});
+	},
+	thick_box_height_fix : function() {
+		var self = this;
+		jQuery( window ).resize(function() {
+			self.fix_thickbox_height();
+		});
+	},
+	fix_thickbox_height : function() {
+		var tb_window_height = jQuery( '#TB_window' ).height();
+		var tb_title_height = jQuery( '#TB_title' ).height();
+		jQuery( '#TB_ajaxContent' ).height( tb_window_height - tb_title_height - 18 );
+	}
+}
+
+jQuery( document ).ready( function() {
+	new wfim_font_selector();
+});
